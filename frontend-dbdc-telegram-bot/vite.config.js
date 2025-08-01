67a6d1d3e69285a237875391d80f16e6073e5a9f
Infernaal <95101990+Infernaal@users.noreply.github.com>
2025-08-02 00:53:52 +0300

    Update vite.config.js

--- frontend-dbdc-telegram-bot/vite.config.js
+++ frontend-dbdc-telegram-bot/vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +13,22 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: './', // важно, если ты деплоишь как статик сайт в подкаталог
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash][extname]`
+      }
+    }
+  },
+  server: {
+    // Если ты разрабатываешь и хочешь отключить кэш
+    headers: {
+      'Cache-Control': 'no-store'
+    }
+  }
 })
