5517ee210c940050f950f4d969b97863a75e1c9b
Infernaal <95101990+Infernaal@users.noreply.github.com>
2025-08-03 00:11:55 +0300

    Update ProfileOverlay.vue

--- frontend-dbdc-telegram-bot/src/components/ProfileOverlay.vue
+++ frontend-dbdc-telegram-bot/src/components/ProfileOverlay.vue
@@ -497,8 +497,8 @@ const overlayStyle = {
 }
 
 const triangleStyle = computed(() => {
-  const offsetLeft = triggerPosition.left + triggerPosition.width / 2 - 12
-  const offsetBottom = triggerPosition.bottom ?? 88
+  const offsetLeft = props.triggerPosition.left + props.triggerPosition.width / 2 - 12
+  const offsetBottom = props.triggerPosition.bottom ?? 88
 
   return {
     left: `${offsetLeft}px`,
