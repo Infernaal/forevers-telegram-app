82807afb6b5aee05780735d482fe3bf771d09ca5
Infernaal <95101990+Infernaal@users.noreply.github.com>
2025-08-03 00:09:52 +0300

    Update ProfileOverlay.vue

--- frontend-dbdc-telegram-bot/src/components/ProfileOverlay.vue
+++ frontend-dbdc-telegram-bot/src/components/ProfileOverlay.vue
@@ -488,20 +488,13 @@ const props = defineProps({
   },
   triggerPosition: {
     type: Object,
-    default: () => ({ left: 0, width: 0 })
+    required: true
   }
 })
 
 const overlayStyle = {
   bottom: 'calc(88px + env(safe-area-inset-bottom, 0px) + 15px)'
 }
-
-defineProps({
-  triggerPosition: {
-    type: Object,
-    required: true
-  }
-})
 
 const triangleStyle = computed(() => {
   const offsetLeft = triggerPosition.left + triggerPosition.width / 2 - 12
@@ -511,7 +504,7 @@ const triangleStyle = computed(() => {
     left: `${offsetLeft}px`,
     bottom: `calc(${offsetBottom}px + env(safe-area-inset-bottom, 0px))`
   }
-}
+})
 
 // Emits
 const emit = defineEmits(['close'])
