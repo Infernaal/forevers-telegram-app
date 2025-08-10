3a433fdc5586b8eb37e840d5723cd944c6efe2c9
Infernaal <95101990+Infernaal@users.noreply.github.com>
2025-08-11 00:45:17 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -41,6 +41,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https:\/\/.*\.fly\.dev"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
